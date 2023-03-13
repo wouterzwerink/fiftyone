cc51920c4040f29a3de756099113ee9138d10577
Lanny W <lanzhenwang9@gmail.com>
2023-03-13 10:47:13 -0500

    save last used brain key for similarity search and use it as default method

--- app/packages/core/src/components/Actions/similar/utils.ts
+++ app/packages/core/src/components/Actions/similar/utils.ts
@@ -117,6 +117,7 @@ export const useSortBySimilarity = (close) => {
             set(fos.savedLookerOptions, (cur) => ({ ...cur, showJSON: false }));
             set(fos.hiddenLabels, {});
             set(fos.modal, null);
+            set(searchBrainKeyValue, combinedParameters.brainKey);
             close();
 
             return data;
